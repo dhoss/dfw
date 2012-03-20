979d7d8f88c10ef6dadbe13e48022a59873e1bbd
Devin Austin <dhoss@cpan.org>
2012-03-20 17:32:54 -0600

    added JSON view

---
+++ Dfw/lib/Dfw/View/JSON.pm
@@ -0,0 +1,29 @@
+package Dfw::View::JSON;
+
+use strict;
+use base 'Catalyst::View::JSON';
+
+=head1 NAME
+
+Dfw::View::JSON - Catalyst JSON View
+
+=head1 SYNOPSIS
+
+See L<Dfw>
+
+=head1 DESCRIPTION
+
+Catalyst JSON View.
+
+=head1 AUTHOR
+
+Devin Austin
+
+=head1 LICENSE
+
+This library is free software, you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
---
+++ Dfw/t/view_JSON.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+BEGIN { use_ok 'Dfw::View::JSON' }
+
+done_testing();
